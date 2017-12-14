4d5177a9a5e8d11f232bc4f01a9c33461fba713b
mmatthews06 <mmatthews06@gmail.com>
2017-12-13 22:32:52 -0500

    add some linting via scalastyle (used via IntelliJ plugin for now)

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,7 @@
+// Copyright (C) 2017 Michael Matthews.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
 
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.ml.Pipeline
@@ -6,9 +10,10 @@ import org.apache.spark.ml.feature.VectorAssembler
 
 object Main {
 
+  val spark: SparkSession = SparkSession.builder().getOrCreate()
+
   def main(args: Array[String] = Array()): Unit = {
-    val fileName = if (args.length > 0) args(0) else "./data/german_credit.csv";
-    val spark = SparkSession.builder().getOrCreate()
+    val fileName = if (args.length > 0) args(0) else "./data/german_credit.csv"
 
     val data = spark.read
       .option("header", "true")
@@ -23,6 +28,10 @@ object Main {
 
   def logisticRegression(data: DataFrame) {
     // Really poor steps for logistic regression.
+    import spark.implicits._
+    val seed = 42
+    val trainSplit = 0.70
+    val testSplit = 0.30
     val featuresAndLabels = data.select(
       data("Creditability").as("label"),
       $"Account Balance",
@@ -50,7 +59,7 @@ object Main {
         )
         .setOutputCol("features")
 
-    val Array(training, test) = cleanData.randomSplit(Array(0.70, 0.30), seed=42)
+    val Array(training, test) = cleanData.randomSplit(Array(trainSplit, testSplit), seed=seed)
 
     val lr = new LogisticRegression()
     val pipeline = new Pipeline().setStages(Array(featureExtractor, lr))
