673890704b03d615845e7e9f0f30f227c9e7f807
mmatthews06 <mmatthews06@gmail.com>
2017-12-16 19:12:30 -0500

    logistic regression - move logreg run to its own file

---
+++ src/main/scala/ml/LogisticRegressionModel.scala
@@ -0,0 +1,57 @@
+// Copyright (C) 2017 Michael Matthews.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+package ml
+
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+object LogisticRegressionModel {
+  val spark: SparkSession = SparkSession.builder().getOrCreate()
+  import spark.implicits._
+
+  def run(data: DataFrame): DataFrame = {
+    val seed = 42
+    val trainSplit = 0.70
+    val testSplit = 0.30
+
+    val classifier = new LogisticRegression()
+    val pipelineBuilder = new PipelineBuilder()
+      .oneHot("Account Balance", "balanceOH")
+      .oneHot("Guarantors", "guarantorsOH")
+      .oneHot("Payment Status of Previous Credit", "paymentStatusOH")
+      .oneHot("Sex & Marital Status", "maritalStatusOH")
+      .oneHot("Concurrent Credits", "currentCreditOH")
+      .features(
+        "balanceOH",
+        "paymentStatusOH",
+        "maritalStatusOH",
+        "guarantorsOH",
+        "currentCreditOH",
+        "Duration of Credit (month)",
+        "Credit Amount",
+        "Age (years)"
+      )
+      .model(classifier)
+
+    val Array(training, test) = data.select(
+        data("Creditability").as("label"),
+        $"Account Balance",
+        $"Duration of Credit (month)",
+        $"Payment Status of Previous Credit",
+        $"Credit Amount",
+        $"Sex & Marital Status",
+        $"Guarantors",
+        $"Age (years)",
+        $"Concurrent Credits"
+      )
+      .na.drop()
+      .randomSplit(Array(trainSplit, testSplit), seed=seed)
+
+    val pipeline = new Pipeline().setStages(pipelineBuilder.build())
+    val model = pipeline.fit(training)
+    model.transform(test)
+  }
+}
--- src/main/scala/ml/Main.scala
+++ src/main/scala/ml/Main.scala
@@ -4,11 +4,7 @@
 //
 package ml
 
-import ml.FeatureEngineering._
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.classification.LogisticRegression
-import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.mllib.evaluation.MulticlassMetrics
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
@@ -30,7 +26,8 @@ object Main {
 
     // Logistic Regression Model
     println("\n" + "-" * 10 + " Logistic Regression " + "-" * 10) // scalastyle:ignore
-    logisticRegression(data)
+    val logisticRegressionResults = LogisticRegressionModel.run(data)
+    evaluateModel(logisticRegressionResults)
 
     // Decision Tree Model
     println("\n" + "-" * 10 + " Decision Tree " + "-" * 10) // scalastyle:ignore
@@ -48,50 +45,6 @@ object Main {
     pca.show()
 
     spark.stop()
-  }
-
-  def logisticRegression(data: DataFrame) {
-    val seed = 42
-    val trainSplit = 0.70
-    val testSplit = 0.30
-
-    val balanceEncoder = "Account Balance" oneHot "balanceOH"
-    val guarantorEncoder = "Guarantors" oneHot "guarantorsOH"
-    val paymentStatusEncoder = "Payment Status of Previous Credit" oneHot "paymentStatusOH"
-    val maritalStatusEncoder = "Sex & Marital Status" oneHot "maritalStatusOH"
-    val currentCreditEncoder = "Concurrent Credits" oneHot "currentCreditOH"
-
-    val featuresAndLabels = data.select(
-      data("Creditability").as("label"), $"Account Balance", $"Duration of Credit (month)",
-      $"Payment Status of Previous Credit", $"Credit Amount", $"Sex & Marital Status", $"Guarantors", $"Age (years)",
-      $"Concurrent Credits"
-    )
-
-    val cleanData = featuresAndLabels.na.drop()
-    val Array(training, test) = cleanData.randomSplit(Array(trainSplit, testSplit), seed=seed)
-
-    val featureExtractor = new VectorAssembler()
-        .setInputCols(Array(
-          "balanceOH", "paymentStatusOH", "maritalStatusOH", "guarantorsOH", "currentCreditOH",
-          "Duration of Credit (month)", "Credit Amount", "Age (years)"
-        ))
-        .setOutputCol("features")
-
-    val lr = new LogisticRegression()
-    val pipeline = new Pipeline().setStages(Array(
-      balanceEncoder,
-      guarantorEncoder,
-      paymentStatusEncoder,
-      maritalStatusEncoder,
-      currentCreditEncoder,
-      featureExtractor,
-      lr
-    ))
-
-    val model = pipeline.fit(training)
-    val results = model.transform(test)
-
-    evaluateModel(results)
   }
 
   def evaluateModel(results: DataFrame): Unit = {
