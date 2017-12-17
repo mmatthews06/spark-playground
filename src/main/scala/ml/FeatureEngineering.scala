fa961b59b3908e197fe566a32e49441aa27478ad
mmatthews06 <mmatthews06@gmail.com>
2017-12-16 20:42:32 -0500

    refactor - move vectorassmble to feature engineering and remove superfluous code in PipelineBuilder

--- src/main/scala/ml/FeatureEngineering.scala
+++ src/main/scala/ml/FeatureEngineering.scala
@@ -4,10 +4,18 @@
 //
 package ml
 
-import org.apache.spark.ml.feature.OneHotEncoder
+import org.apache.spark.ml.feature.{OneHotEncoder, VectorAssembler}
 
 object FeatureEngineering {
   implicit class StringEngineering(col1: String) {
     def oneHot(col2: String): OneHotEncoder = new OneHotEncoder().setInputCol(col1).setOutputCol(col2)
+  }
+
+  implicit class VectorAssemblerEngineering(list1: Seq[String]) {
+    def extract(): VectorAssembler = {
+      new VectorAssembler()
+        .setInputCols(list1.toArray)
+        .setOutputCol("features")
+    }
   }
 }
--- src/main/scala/ml/PipelineBuilder.scala
+++ src/main/scala/ml/PipelineBuilder.scala
@@ -7,32 +7,15 @@ package ml
 
 import ml.FeatureEngineering._
 import org.apache.spark.ml.PipelineStage
-import org.apache.spark.ml.feature.VectorAssembler
-
 
 class PipelineBuilder(pipeline: List[PipelineStage] = List[PipelineStage]()) {
 
-  def model(model: PipelineStage): PipelineBuilder = {
-    val newPipeline = pipeline :+ model
-    new PipelineBuilder(newPipeline)
-  }
+  def build(): Array[PipelineStage] = pipeline.toArray
 
-  def oneHot(col1: String, col2:String): PipelineBuilder = {
-    val encoder = col1 oneHot col2
-    val newPipeline = pipeline :+ encoder
-    new PipelineBuilder(newPipeline)
-  }
+  def features(features: String*): PipelineBuilder = new PipelineBuilder(pipeline :+ features.extract)
 
-  def features(features: String*): PipelineBuilder = {
-    val featureExtractor = new VectorAssembler()
-      .setInputCols(features.toArray)
-      .setOutputCol("features")
+  def model(model: PipelineStage): PipelineBuilder = new PipelineBuilder(pipeline :+ model)
 
-    val newPipeline = pipeline :+ featureExtractor
-    new PipelineBuilder(newPipeline)
-  }
+  def oneHot(col1: String, col2:String): PipelineBuilder = new PipelineBuilder(pipeline :+ (col1 oneHot col2))
 
-  def build(): Array[PipelineStage] = {
-    pipeline.toArray
-  }
 }
