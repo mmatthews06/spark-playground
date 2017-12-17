34eb2941e3360b4cf6a914c2bde631e12b9be60b
mmatthews06 <mmatthews06@gmail.com>
2017-12-16 19:07:06 -0500

    decision tree - refactor code to use PipelineBuilder, and fewer unused variables

--- src/main/scala/ml/DecisionTreeModel.scala
+++ src/main/scala/ml/DecisionTreeModel.scala
@@ -6,9 +6,7 @@ package ml
 
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.DecisionTreeClassifier
-import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.sql.{DataFrame, SparkSession}
-import FeatureEngineering._
 
 object DecisionTreeModel {
   val spark: SparkSession = SparkSession.builder().getOrCreate()
@@ -19,39 +17,40 @@ object DecisionTreeModel {
     val trainSplit = 0.70
     val testSplit = 0.30
 
-    val balanceEncoder = "Account Balance" oneHot "balanceOH"
-    val guarantorEncoder = "Guarantors" oneHot "guarantorsOH"
-    val paymentStatusEncoder = "Payment Status of Previous Credit" oneHot "paymentStatusOH"
-    val maritalStatusEncoder = "Sex & Marital Status" oneHot "maritalStatusOH"
-    val currentCreditEncoder = "Concurrent Credits" oneHot "currentCreditOH"
+    val classifier = new DecisionTreeClassifier()
+    val pipelineBuilder = new PipelineBuilder()
+      .oneHot("Account Balance", "balanceOH")
+      .oneHot("Guarantors", "guarantorsOH")
+      .oneHot("Payment Status of Previous Credit", "paymentStatusOH")
+      .oneHot("Sex & Marital Status", "maritalStatusOH")
+      .oneHot("Concurrent Credits", "currentCreditOH")
+      .features(
+        "balanceOH",
+        "paymentStatusOH",
+        "maritalStatusOH",
+        "guarantorsOH",
+        "currentCreditOH",
+        "Duration of Credit (month)",
+        "Credit Amount",
+        "Age (years)"
+      )
+      .model(classifier)
 
-    val featuresAndLabels = data.select(
-      data("Creditability").as("label"), $"Account Balance", $"Duration of Credit (month)",
-      $"Payment Status of Previous Credit", $"Credit Amount", $"Sex & Marital Status", $"Guarantors", $"Age (years)",
-      $"Concurrent Credits"
-    )
+    val Array(training, test) = data.select(
+        data("Creditability").as("label"),
+        $"Account Balance",
+        $"Duration of Credit (month)",
+        $"Payment Status of Previous Credit",
+        $"Credit Amount",
+        $"Sex & Marital Status",
+        $"Guarantors",
+        $"Age (years)",
+        $"Concurrent Credits"
+      )
+      .na.drop()
+      .randomSplit(Array(trainSplit, testSplit), seed=seed)
 
-    val cleanData = featuresAndLabels.na.drop()
-    val Array(training, test) = cleanData.randomSplit(Array(trainSplit, testSplit), seed=seed)
-
-    val featureExtractor = new VectorAssembler()
-      .setInputCols(Array(
-        "balanceOH", "paymentStatusOH", "maritalStatusOH", "guarantorsOH", "currentCreditOH",
-        "Duration of Credit (month)", "Credit Amount", "Age (years)"
-      ))
-      .setOutputCol("features")
-
-    val decisionTree = new DecisionTreeClassifier()
-    val pipeline = new Pipeline().setStages(Array(
-      balanceEncoder,
-      guarantorEncoder,
-      paymentStatusEncoder,
-      maritalStatusEncoder,
-      currentCreditEncoder,
-      featureExtractor,
-      decisionTree
-    ))
-
+    val pipeline = new Pipeline().setStages(pipelineBuilder.build())
     val model = pipeline.fit(training)
     model.transform(test)
   }
