b84fac3670203c1aaf8baec2b4d81d34f5a57b9d
mmatthews06 <mmatthews06@gmail.com>
2017-12-17 22:34:37 -0500

    models - refactor to use base model to get "standard" pipeline and "standard" features.

--- src/main/scala/ml/RandomForestModel.scala
+++ src/main/scala/ml/RandomForestModel.scala
@@ -6,52 +6,21 @@ package ml
 
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.RandomForestClassifier
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.DataFrame
 
-object RandomForestModel {
-  val spark: SparkSession = SparkSession.builder().getOrCreate()
-  import spark.implicits._
+object RandomForestModel extends BaseModel {
 
   def run(data: DataFrame): DataFrame = {
-    val seed = 42
-    val trainSplit = 0.70
-    val testSplit = 0.30
-
     val classifier = new RandomForestClassifier()
-    val pipelineBuilder = new PipelineBuilder()
-      .oneHot("Account Balance", "balanceOH")
-      .oneHot("Guarantors", "guarantorsOH")
-      .oneHot("Payment Status of Previous Credit", "paymentStatusOH")
-      .oneHot("Sex & Marital Status", "maritalStatusOH")
-      .oneHot("Concurrent Credits", "currentCreditOH")
-      .features(
-        "balanceOH",
-        "paymentStatusOH",
-        "maritalStatusOH",
-        "guarantorsOH",
-        "currentCreditOH",
-        "Duration of Credit (month)",
-        "Credit Amount",
-        "Age (years)"
-      )
+
+    val pipelineBuilder = getStdPipeline()
       .model(classifier)
 
-    val Array(training, test) = data.select(
-        data("Creditability").as("label"),
-        $"Account Balance",
-        $"Duration of Credit (month)",
-        $"Payment Status of Previous Credit",
-        $"Credit Amount",
-        $"Sex & Marital Status",
-        $"Guarantors",
-        $"Age (years)",
-        $"Concurrent Credits"
-      )
-      .na.drop()
-      .randomSplit(Array(trainSplit, testSplit), seed=seed)
+    val Array(training, test) = getStdFeatures(data)
 
     val pipeline = new Pipeline().setStages(pipelineBuilder.build())
     val model = pipeline.fit(training)
     model.transform(test)
   }
+
 }
