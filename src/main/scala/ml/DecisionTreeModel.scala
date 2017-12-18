5767efa460781ede3d24693b731b8f030353145a
mmatthews06 <mmatthews06@gmail.com>
2017-12-18 01:00:21 -0500

    decision tree - parameter grid for maxDepth

--- src/main/scala/ml/DecisionTreeModel.scala
+++ src/main/scala/ml/DecisionTreeModel.scala
@@ -6,15 +6,27 @@ package ml
 
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.DecisionTreeClassifier
+import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
+import org.apache.spark.ml.tuning.{ParamGridBuilder, TrainValidationSplit}
 import org.apache.spark.sql.DataFrame
 
 object DecisionTreeModel extends BaseModel {
 
   def run(data: DataFrame): DataFrame = {
+    // scalastyle:off magic.number
     val classifier = new DecisionTreeClassifier()
+    val paramGrid = new ParamGridBuilder()
+      .addGrid(classifier.maxDepth, Array(1, 3, 5, 10, 15, 20, 25))
+      .build()
+
+    val trainValidationSplit = new TrainValidationSplit()
+      .setEstimator(classifier)
+      .setEvaluator(new BinaryClassificationEvaluator())
+      .setEstimatorParamMaps(paramGrid)
+      .setTrainRatio(0.8)
 
     val pipelineBuilder = getStdPipeline()
-      .model(classifier)
+      .model(trainValidationSplit)
 
     val Array(training, test) = getStdFeatures(data)
 
